af430ee563a5387fd8a0e75e6c5512567a8f3ab7
DESKTOP-FPTSOG5\DarkTuan <bachdangtuan93@gmail.com>
2023-03-09 13:41:16 +0700

    first commit

---
+++ check-backup-db.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+# $? thực thi không thành công trả về mã thoát là 1, thực thi thành công mã thoát là 0
+grep "example" testfile.txt
+
+case $? in
+  1)
+   echo 'thực hiện không thành công'
+   ;;
+  0)
+  echo 'thực hiện thành công'
+   ;;
+ *)
+   STATEMENTS
+   ;;
+esac
+
