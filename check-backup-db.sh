9ce9fe4cd7fb0c1d6e309553ba45c6e620799675
DESKTOP-FPTSOG5\DarkTuan <bachdangtuan93@gmail.com>
2023-03-09 13:49:05 +0700

    no message

--- check-backup-db.sh
+++ check-backup-db.sh
@@ -1,7 +1,25 @@
 #!bin/bash
 
-# $? thực thi không thành công trả về mã thoát là 1, thực thi thành công mã thoát là 0
-grep "example" testfile.txt
+# $? thực thi không thành công trả về mã thoát là 1, thực thi thành công mã thoát là 0, đối tượng thực thi không có 
+# trả về số khác 0
+# 
+
+# grep "example" testfile.txt
+
+# echo $?
+
+# case $? in
+#   1)
+#    echo 'thực hiện không thành công'
+#    ;;
+#   0)
+#   echo 'thực hiện thành công'
+#    ;;
+#   *)
+#    echo 'không có kết quả'
+#    ;;
+# esac
+ping c -1 8.8.8.8 
 
 echo $?
 
