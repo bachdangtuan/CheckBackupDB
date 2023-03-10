037f6164d8821be31400743c506dfd2e2044115a
DESKTOP-FPTSOG5\DarkTuan <bachdangtuan93@gmail.com>
2023-03-10 11:23:41 +0700

    no message

--- config.sh
+++ config.sh
@@ -4,4 +4,4 @@ TOKEN="6112203391:AAEuDTYX3KQRNuoLKuJ0NAtpRoamdHIQQkA"
 CHAT_ID="-957135587"
 # URL API post telegeram
 URL="https://api.telegram.org/bot${TOKEN}/sendMessage"
-export db=testbackup;
+db=testbackup;
