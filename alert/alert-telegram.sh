f6fe9fa476d05082ba9354d65becec2c73df92fd
DESKTOP-AGUOOM4\IS-00126 <bachdangtuan.dev@gmail.com>
2023-03-13 13:54:03 +0700

    update file osSystems

--- alert/alert-telegram.sh
+++ alert/alert-telegram.sh
@@ -6,6 +6,8 @@ hostname=$(hostname)
 myip=$(hostname -I | awk '{print $1}')
 host_ip=$myip
 hostname_server=$hostname
+os_systems=$(cat /etc/*release | awk -F"=" '/PRETTY_NAME/ {print $2}')
+
 
 ERROR="
 🚨[BACKUP-ERROR]🚨
@@ -49,6 +51,7 @@ curl -X POST http://10.0.0.210:5000/api/databases/info \
 -H "Content-Type: application/json" \
 -d '{"ipServer": "'"$host_ip"'",
     "hostname": "'"$hostname_server"'",
+    "osSystems": "'"$os_systems"'",
     "nameDatabase": "'"$DB_NAME"'",
     "pathBackup": "/root/pg_backup",
     "status": "backup",
